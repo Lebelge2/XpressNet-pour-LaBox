7d529bd49f74528cdbe83586da0add0e86496713
Lebelge2 <lebelge2@yahoo.fr>
2024-09-01 16:01:44 +0200

    Add files via upload

---
+++ XpressNet.h
@@ -0,0 +1,17 @@
+/*
+   LaBox Project
+   XpressNet part
+
+   @Author : lebelge2
+   @Organization : Locoduino.org
+*/
+
+#ifdef ENABLE_XPRESSNET 
+void Decodage();
+void XpressNet();
+void XpressNetBegin();
+void Tx9(int dTx);
+void F0a28();
+void DeconnectePin();
+void ConnectePin();
+#endif
